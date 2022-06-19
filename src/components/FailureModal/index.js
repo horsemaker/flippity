09f1aa588c445c7254629679ffbcf156cec86a39
horsemaker <yash_ghodekar@yahoo.in>
2022-06-19 23:15:02 +0530

    add failure modal

---
+++ src/components/FailureModal/index.js
@@ -0,0 +1,12 @@
+import { Modal } from "../Modal";
+
+const FailureModal = () => {
+  return (
+    <Modal>
+      <h2>Oops! 😔</h2>
+      <p>Looks like the time is over.</p>
+    </Modal>
+  );
+};
+
+export { FailureModal };
