f4aaab0a79c2b679f5ce93f4466bf167e61a4d0f
horsemaker <yash_ghodekar@yahoo.in>
2022-06-20 00:00:01 +0530

    feat: add play again button

--- src/components/FailureModal/index.js
+++ src/components/FailureModal/index.js
@@ -1,10 +1,34 @@
+import { RESET_QUIZ } from "../../constants";
+import { useQuiz, useQuizzes } from "../../contexts";
 import { Modal } from "../Modal";
 
 const FailureModal = () => {
+  const { quizzes } = useQuizzes();
+  const { dispatchQuiz } = useQuiz();
+
   return (
     <Modal>
       <h2>Oops! 😔</h2>
       <p>Looks like the time is over.</p>
+      <button
+        className="modal-action__button"
+        onClick={() =>
+          dispatchQuiz({
+            type: RESET_QUIZ,
+            payload: {
+              index: 0,
+              data: [...quizzes[0]],
+              turns: 0,
+              score: 0,
+              time: 180,
+              isTimerActive: false,
+              haveCompleted: false,
+            },
+          })
+        }
+      >
+        Play again
+      </button>
     </Modal>
   );
 };
--- src/components/SuccessModal/index.js
+++ src/components/SuccessModal/index.js
@@ -1,11 +1,34 @@
 import React from "react";
+import { RESET_QUIZ } from "../../constants";
+import { useQuiz, useQuizzes } from "../../contexts";
 import { Modal } from "../Modal";
 
 const SuccessModal = () => {
+  const { quizzes } = useQuizzes();
+  const { dispatchQuiz } = useQuiz();
+
   return (
     <Modal>
       <h2>Yay! 🥳</h2>
       <p>You have successfully solved all the patterns.</p>
+      <button className="modal-action__button"
+        onClick={() =>
+          dispatchQuiz({
+            type: RESET_QUIZ,
+            payload: {
+              index: 0,
+              data: [...quizzes[0]],
+              turns: 0,
+              score: 0,
+              time: 180,
+              isTimerActive: false,
+              haveCompleted: false,
+            },
+          })
+        }
+      >
+        Play again
+      </button>
     </Modal>
   );
 };
